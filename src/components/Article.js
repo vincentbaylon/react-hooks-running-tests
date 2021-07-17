5697c2c706936b5dc0bd8dcdeeea1065462da8b6
vincentbaylon <vincentbaylon@gmail.com>
2021-07-17 16:16:49 -0700

    commit tree 50aeeb6f1424081e98e90397284d54b319e0524c

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,8 +3,7 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* uncomment the line below to pass the test */}
-      {/* please pass this test */}
+      {"please pass this test"}
     </div>
   );
 }
